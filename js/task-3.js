28d83ef32a949f5566d25bffbe85a8545dd67732
seroga <sergijvchichur@gmail.com>
2026-04-09 22:01:39 +0300

    init

--- js/task-3.js
+++ js/task-3.js
@@ -1,4 +1,4 @@
-const sortByDescendingFriendCount = users => users.sort((a, b) => b.friends.length - a.friends.length);
+const sortByDescendingFriendCount = users => users.toSorted((a, b) => b.friends.length - a.friends.length);
 
 console.log(
     sortByDescendingFriendCount([
